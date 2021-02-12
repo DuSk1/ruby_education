10891bf47c9fbec642d6a7326c08d98fa334963a
dusk1986@gmail.com <dusk1986@gmail.com>
2021-02-12 14:35:08 +0700

    #master Added guess_number.rb

---
+++ guess_number.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+number_to_guess = rand(1..100)
+
+1.upto(1000) do |try_number|
+  puts "Try number: #{try_number}"
+
+  print 'Try to guess number (1 - 100): '
+  users_number = gets.to_i
+
+  if number_to_guess == users_number
+    print "You guess! #{number_to_guess} \n"
+    exit(0)
+  elsif  number_to_guess > users_number
+    print "Number is bigger.\n"
+  elsif  number_to_guess < users_number
+    print "Number is smaller.\n"
+  end
+
+end
+
+# print "#{number_to_guess}\n"
